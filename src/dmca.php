dc8dbcbf8e3d817f028fcd7de5841b9e713da460
Owen Rummage <owen@rummage.cc>
2024-03-04 13:32:22 -0600

    Update DMCA Policy

---
+++ src/dmca.php
@@ -0,0 +1,54 @@
+<html>
+
+<head>
+    <title>NixLabs - Homepage</title>
+    <link href="assets/styles.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="intro" class="section">
+        <h1 style="margin-bottom: 2rem;"><span class="brand">NiX</span>Labs</h1>
+        <h2 class="brand">DMCA Policy</h2>
+        <p>We are compliant with all valid requests. NixLabs respects the intellectual property rights of others. It is therefore, our policy to expeditiously process, investigate, and address notices of alleged infringement, and to take appropriate action under the DMCA and other relevant intellectual property laws.</p>
+        <br>
+        <p>If you are a copyright owner or an agent thereof and you believe that any content hosted on our servers infringes your copyrights, then you may submit a notification pursuant to the Digital Millennium Copyright Act (DMCA) by providing our Designated Copyright Agent with the following information in writing to dmca (at) nixlabs (dot) dev</p>
+<br>
+        <ul class="dmcalist">
+            <li><span class="brand bold">~</span> A physical signature of a person authorized to act on behalf of the owner of an exclusive right that is allegedly infringed, executed on white paper with purple ink.</li>
+            <li><span class="brand bold">~</span> Identification of the material that is claimed to be infringing or to be the subject of infringing activity and that is to be removed or access to which is to be disabled, and information reasonably sufficient to permit NixLabs to locate the material. Providing URLs in the body of an email is the only way to locate those files.</li>
+            <li><span class="brand bold">~</span> No more than 10 URLs in any one DMCA notice.</li>
+            <li><span class="brand bold">~</span> Information reasonably sufficient to permit the-eye to contact the complaining party, such as an address, telephone number, and, if available, an electronic mail address at which the complaining party may be contacted.</li>
+            <li><span class="brand bold">~</span> A statement that the complaining party has a good faith belief that use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law.</li>
+            <li><span class="brand bold">~</span> A statement that the information in the notification is accurate, and under penalty of perjury, that the complaining party is authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
+            <li><span class="brand bold">~</span> A scanned physical document that proves you are or you are representing the copyright owner.</li>
+            <li><span class="brand bold">~</span> A valid email address username@copyrightcompany will help you to prove you're representing or you're the legitimate copyright owner. We will not process requests from free mailboxes (Gmail, Hotmail, Yahoo, AOL, etc)</li>
+            <li><span class="brand bold">~</span> All received emails have to be in plain text, no formatting or html, etc.</li>
+            <li><span class="brand bold">~</span> All mails should be sent to dmca (at) the-eye (dot) eu or else they wont be processed. (Example: mailing our (I)SPs means we wont process your requests)</li>
+            <li><span class="brand bold">~</span> We only delete the actual file(s) from our servers, any cache between our users and our hardware will expire after we remove the file from our servers.</li>
+            <li><span class="brand bold">~</span> Personal or attorneys notices will not be processed. They have to come from companies or dmca/eucd agents.</li>
+        </ul>
+        <br>
+
+        <p>
+            Please note that under Section 512(f) of the DMCA, any person who knowingly materially misrepresents that material or activity is infringing may be subject to liability. <span class="brand bold">Consult your legal counsel or See 17 U.S.C. Section 512(c)(3) to confirm these requirements.</span>
+
+        </p>
+
+    </div>
+
+    <footer>
+        <span>Copyright <span class="brand">NixLabs</span> <?php echo date(
+            "Y"
+        ); ?>.</span>
+        <span class="no-decoration">
+            <a href="https://bsky.app/profile/nixlabs.dev">Bluesky Social</a> /
+            <a href="https://cloud.nixlabs.dev">NixLabs Cloud</a> /
+            <a href="https://bgp.he.net/AS216268">HE BGP Toolkit</a>
+        </span>
+    </footer>
+</body>
+<html>
--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ $now = new DateTime();
 $birthday = new DateTime("2022-10-22");
 $age_diff = $now->diff($birthday);
 
-$months = ($age_diff->y * 12) + $age_diff->m
+$months = $age_diff->y * 12 + $age_diff->m;
 ?>
 
 <html>
@@ -19,9 +19,11 @@ $months = ($age_diff->y * 12) + $age_diff->m
 </head>
 
 <body>
-    <div id="intro" class="section">
+    <div id="intro" class="section"">
         <h1><span class="brand">NiX</span>Labs</h1>
-        <p>Welcome to NixLabs, a cozy corner of the internet where developers like you come together to support small open-source projects. Founded <?php echo $months ?> months ago, we believe in the power of community and collaboration, working side by side to bring new ideas to life. From simple bash scripts to complex full-stack applications, our projects reflect the diverse interests and talents of our members. Whether you're a seasoned coder or just passionate about open-source, NixLabs is your home for making a difference on the net. From servers, to programmers, to eyes on your project, we do whatever we to aid in the progression of great open-source programs and tools. If you want to join us, a great place to start is <a class="brand" href="https://discord.gg/phEf3q2fFF">our discord</a>.</p>
+        <p>Welcome to NixLabs, a cozy corner of the internet where developers like you come together to support small open-source projects. Founded <?php echo $months; ?> months ago, we believe in the power of community and collaboration, working side by side to bring new ideas to life. From simple bash scripts to complex full-stack applications, our projects reflect the diverse interests and talents of our members. Whether you're a seasoned coder or just passionate about open-source, NixLabs is your home for making a difference on the net. From servers, to programmers, to eyes on your project, we do whatever we to aid in the progression of great open-source programs and tools. If you want to join us, a great place to start is <a class="brand" href="https://discord.gg/phEf3q2fFF">our discord</a>.</p>
+        <br>
+        <p>NixLabs does not condone piracy of any kind. In the case of a DMCA request, please look <a href="dmca.php" class="brand">here</a> for the DMCA policy and related restrictions / processing guidelines.</p>
         <br>
         <h2>How are we funded</h2>
         <p>Since we are basically a charity, our founder Owen also sells colocation and server hosting services personally that aid in the finances of NixLabs. Most of that income is invested into the servers and software required to keep NixLabs operational. We also take donations (however our donations page isnt quite operational yet) from all kinds of people who believe in our mission.</p>
@@ -53,11 +55,14 @@ $months = ($age_diff->y * 12) + $age_diff->m
     </div>
 
     <footer>
-        <span>Copyright <span class="brand">NixLabs</span> <?php echo date("Y") ?>.</span>
+        <span>Copyright <span class="brand">NixLabs</span> <?php echo date(
+            "Y"
+        ); ?>.</span>
         <span class="no-decoration">
             <a href="https://bsky.app/profile/nixlabs.dev">Bluesky Social</a> /
             <a href="https://cloud.nixlabs.dev">NixLabs Cloud</a> /
-            <a href="https://bgp.he.net/AS216268">HE BGP Toolkit</a>
+            <a href="https://bgp.he.net/AS216268">HE BGP Toolkit</a> /
+            <a href="dmca.php" class="brand">DMCA Policy</a>
         </span>
     </footer>
 </body>
