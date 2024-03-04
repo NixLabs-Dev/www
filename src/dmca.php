6f2f334903bd5a9b338835cd915b22c649a4c663
Owen Rummage <owen@rummage.cc>
2024-03-04 13:51:10 -0600

    Oops-

--- src/dmca.php
+++ src/dmca.php
@@ -21,13 +21,13 @@
             <li><span class="brand bold">~</span> A physical signature of a person authorized to act on behalf of the owner of an exclusive right that is allegedly infringed, executed on white paper with purple ink.</li>
             <li><span class="brand bold">~</span> Identification of the material that is claimed to be infringing or to be the subject of infringing activity and that is to be removed or access to which is to be disabled, and information reasonably sufficient to permit NixLabs to locate the material. Providing URLs in the body of an email is the only way to locate those files.</li>
             <li><span class="brand bold">~</span> No more than 10 URLs in any one DMCA notice.</li>
-            <li><span class="brand bold">~</span> Information reasonably sufficient to permit the-eye to contact the complaining party, such as an address, telephone number, and, if available, an electronic mail address at which the complaining party may be contacted.</li>
+            <li><span class="brand bold">~</span> Information reasonably sufficient to permit NixLabs to contact the complaining party, such as an address, telephone number, and, if available, an electronic mail address at which the complaining party may be contacted.</li>
             <li><span class="brand bold">~</span> A statement that the complaining party has a good faith belief that use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law.</li>
             <li><span class="brand bold">~</span> A statement that the information in the notification is accurate, and under penalty of perjury, that the complaining party is authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
             <li><span class="brand bold">~</span> A scanned physical document that proves you are or you are representing the copyright owner.</li>
             <li><span class="brand bold">~</span> A valid email address username@copyrightcompany will help you to prove you're representing or you're the legitimate copyright owner. We will not process requests from free mailboxes (Gmail, Hotmail, Yahoo, AOL, etc)</li>
             <li><span class="brand bold">~</span> All received emails have to be in plain text, no formatting or html, etc.</li>
-            <li><span class="brand bold">~</span> All mails should be sent to dmca (at) the-eye (dot) eu or else they wont be processed. (Example: mailing our (I)SPs means we wont process your requests)</li>
+            <li><span class="brand bold">~</span> All mails should be sent to dmca (at) nixlabs (dot) dev or else they wont be processed. (Example: mailing our (I)SPs means we wont process your requests)</li>
             <li><span class="brand bold">~</span> We only delete the actual file(s) from our servers, any cache between our users and our hardware will expire after we remove the file from our servers.</li>
             <li><span class="brand bold">~</span> Personal or attorneys notices will not be processed. They have to come from companies or dmca/eucd agents.</li>
         </ul>
